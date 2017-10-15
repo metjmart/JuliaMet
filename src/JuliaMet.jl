af3dd710218c6a278a76701bbd63026801b1be5f
Jonathan Martinez <jon.martinez@colostate.edu>
2017-10-15 16:26:12 -0600

    Update for v0.6.0 changes

--- src/JuliaMet.jl
+++ src/JuliaMet.jl
@@ -1,36 +1,30 @@
 module JuliaMet
 
-    using NetCDF, DataStructures, Grid, Interpolations, Colors, PyPlot
-    plt = PyPlot
+    using NetCDF, DataStructures, Interpolations, Colors, PyPlot
 
-    export read_ncvars
-    export finite_dz, finite_dr, finite_dx, finite_dy
-    export newton_cotes, trapz_1d, trapz_2d, trapz_3d
-    export grid_2d, grid_3d, regrid_xy2rt, regrid_xyz2rtz
-    export closest_ind, new_grid2d, new_grid3d, new_regrid_xy2rt, 
-           new_regrid_xyz2rtz, regrid_gfrelxz, regrid_gfrel_xyz
-    export calc_rmw, calc_azmean, uv2urvt, p3swploc
-    export stationary_frame
     export p_centroid
-    export nanmax, nanmin, nansum, nanmean, nanmedian, nanvar, nanstd,
-           nanskewness, nankurtosis
+    export extract_rgbs, create_cmap
+    export calc_rmw, azmean, uv2urvt, p3swploc
+    export finite_dr, finite_dx, finite_dy, finite_dz, finite_laplacian
+    export newton_cotes, trapz1d, trapz2d, trapz3d
+    export nanmax, nanmin, nanextrema, nansum, nanmean, nanmedian, nanvar, 
+           nanstd
+    export read_ncvars
+    export closest_ind, grid2d, grid3d, regrid_xy2rt, regrid_xyz2rtz, 
+           regrid_gfrelxz, regrid_gfrel_xyz
     export obslocs_rtz
-    export create_colormap, extractrgbs, get_rgbs, make_line_colors, kwr_cmap, 
-           kwk_cmap, blue_cmap, blue_r_cmap, red_cmap, red_r_cmap, radar_camp, 
-           radar2_cmap, radar3_cmap, radar_carbone_cmap, radar_carbone2_cmap, 
-           pvwcolors_cmap, pvcolors_cmap, ssec_cmap, bluegie_cmap
-           
+    export stationary_frame
 
-    include("nc_tools.jl")
+
+    include("center_finding.jl")
+    include("cmaps.jl")
+    include("cyclone.jl")
     include("derivative.jl")    
     include("integrate.jl")
-    include("regrid.jl")
-    include("new_regrid.jl")
-    include("cyclone.jl")
-    include("steady_frame.jl")
-    include("center_finding.jl")
     include("nanstats.jl")
+    include("nc_tools.jl")
+    include("regrid.jl")
     include("samurai_obslocs.jl")
-    include("colormaps.jl")
+    include("steady_frame.jl")
 
 end # module
