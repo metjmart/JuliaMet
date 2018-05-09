1f68fb42c9a44bfb5e0fe6c46a8037396af3d1f7
Jonathan Martinez <Jon.Martinez@colostate.edu>
2018-05-09 11:37:24 -0600

    Remove vortex profiles

--- src/cyclone.jl
+++ src/cyclone.jl
@@ -10,8 +10,6 @@
 # 
 # calc_rmw
 # uv2urvt
-# rankine
-# hermite
 # p3swploc
 # *****************************************************************************
 
@@ -73,72 +71,6 @@ function uv2urvt(phi::AbstractVector{<:Real},u::AbstractArray{<:Real},
         vt = -u .* sin.(phi)' + v .* cos.(phi)'
         return ur,vt
     end
-end
-
-#==============================================================================
-rankine
-
-This function will generate a 1-D Rankine vortex for a given rmax and vmax
-*** Assumes r and rmax are in units of meters and vmax is m/s!!!
-===============================================================================#
-
-function rankine(r::AbstractVector{<:Real},rmax::Real,vmax::Real,method::Symbol=:vt)
-
-    if method == :vt
-        return @fastmath [i < rmax ? vmax * i / rmax : rmax * vmax / i for i in r]
-    elseif method == :vort
-        return @fastmath [i < rmax ? 2. * vmax / rmax : 0. for i in r]
-    end
-end
-
-#==============================================================================
-modrankine
-
-This function will generate a 1-D modified Rankine vortex for a given rmax, 
-vmax, and decay parameter (alpha)
-*** Assumes r and rmax are in units of meters, vmax is m/s, and alpha is postive
-===============================================================================#
-
-function modrankine(r::AbstractVector{<:Real},rmax::Real,vmax::Real,alpha::Real)
-    return @fastmath [i < rmax ? vmax * i / rmax : vmax * (i/rmax)^-alpha for i in r]
-end
-
-#==============================================================================
-re87
-
-This function will generate a 1-D vortex profile 
-following Rotunno and Emanuel (1987; JAS; equation 37)
-Need to specify radius array, rmax, r0, vmax, coriolis force
-*** Assumes r, rmax, and r0 are in units of meters, vmax is m/s, and coriolis 
-    force is s^-1
-===============================================================================#
-
-function re87(r::AbstractVector{<:Real},rmax::Real,r0::Real,vmax::Real,fcor::Real)
-
-    vt =  sqrt.( vmax^2 * (r ./ rmax).^2 .* 
-                ( ( (2. * rmax) ./ (r + rmax) ).^3 - ( (2. * rmax) ./ (r0 + rmax) ).^3 ) + 
-                (fcor^2 * r.^2 ./ 4.) ) - (fcor * r) ./ 2.
-end
-
-#==============================================================================
-hermite
-
-Basic cubic Hermite shape function satisfying S(0) = 1, S(1) = 0, and 
-S'(0) = S'(1) = 0.
-Can be used to provide smooth transition regions for, e.g., vorticity rings.
-See Schubert et al. (1999; JAS) for examples
-===============================================================================#
-
-# Single data points
-
-function hermite(radius::Real)
-    return @fastmath 1. - 3. * radius^2 + 2. * radius^3
-end
-
-# Vectors
-
-function hermite(radius::AbstractVector{<:Real})
-    return @fastmath 1. - 3. * radius.^2 + 2. * radius.^3
 end
 
 #==============================================================================
