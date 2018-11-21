10431f9174b2ba064368a95aec5479535a920ca2
Jonathan Martinez <Jon.Martinez@colostate.edu>
2018-11-21 12:05:44 -0700

    Add unstagger function and a regrid_pol2cart method

--- src/JuliaMet.jl
+++ src/JuliaMet.jl
@@ -6,14 +6,15 @@ module JuliaMet
 
     export p_centroid, p_centroid2
     export calc_rmw, uv2urvt, p3swploc
-    export finite_dr, finite_dx, finite_dy, finite_dz, finite_laplacian
+    export finite_dr, finite_dx, finite_dy, finite_dz, finite_laplacian,
+           fd_weights, finite_ds, finite_dsx, finite_dsy, finite_dsz
     export rmnan, fourierols, wavecoeffs
     export newtoncotes, trapz2d, trapz3d
     export nansum, nanmean, nanvar, nanstd, nanmin, nanmax, nanextrema, nanfindmax,
            nanargmax, nanfindmin, nanargmin
     export read_ncvars
     export closest_ind, grid2d, grid3d, xy2rp, regrid_xy2rp, regrid_xyz2rpz,
-           regrid_pol2cart
+           regrid_pol2cart, unstagger
     export obslocs_rtz
     export steadyframe
     export rankine, modrankine, hermite, re87, cw87, wc04
